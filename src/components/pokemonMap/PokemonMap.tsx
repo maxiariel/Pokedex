74a212d28ff0e9ca887b062700e16f25d719deea
Maximiliano Vicentin <maximilianoarielvicentin@gmail.com>
2023-05-22 14:58:41 -0300

    feat: separate component from home

---
+++ src/components/pokemonMap/PokemonMap.tsx
@@ -0,0 +1,40 @@
+import { useContext, useEffect } from "react";
+import { PokemonsContext } from "../../context/PokemonsContext";
+import { IPokemonData } from "../../types";
+import { Link } from "react-router-dom";
+import { Card, Content } from "./StyledPokemonMap";
+
+export default function PokemonMap(): JSX.Element {
+  const { pokemonData, setIsPokemonMap } = useContext(PokemonsContext);
+
+  useEffect(()=>{
+    setIsPokemonMap(true)
+    return (()=>
+    setIsPokemonMap(false))
+  },[])
+
+  return (
+    <>
+      {pokemonData?.map((pokemon: IPokemonData) => {
+        return (
+          <Card key={pokemon.id}>
+            <Link to={`/${pokemon.id}`}>
+              <Content>
+                <p>#{pokemon.id}</p>
+              </Content>
+              <Content>
+                <img
+                  src={`${pokemon.sprites.front_default}`}
+                  alt={pokemon.name}
+                />
+              </Content>
+              <Content>
+                <p>{pokemon.name}</p>
+              </Content>
+            </Link>
+          </Card>
+        );
+      })}
+    </>
+  );
+}
